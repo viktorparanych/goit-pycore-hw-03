00b3489557f05c89002484c99744f35bfe1d615e
viktorparanych <viktorparanych2216@gmail.com>
2026-04-28 13:19:13 +0200

    implement function to calculate days from a given date

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from datetime import date, datetime, timedelta
+
+
+def get_days_from_today(date):
+    try:
+        converted_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today_date = datetime.today().date()
+        delta = (today_date - converted_date).days
+        return delta
+    except ValueError:
+    
+        print("Неправильний формат дати!")
+        return None
+
+print(get_days_from_today("2020-10-09"))
